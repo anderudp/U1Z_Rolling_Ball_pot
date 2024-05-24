70eb1f7f71c781e33101963c5bcb43939d941bb6
anderudp <pappandrasdavid@gmail.com>
2024-05-24 18:59:13 +0200

    lesson 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float sideSpeed = 5f;
     bool canJump = false;
     Rigidbody playerRb;
+    Vector3 startPos = new Vector3(0f, 1f, 5f);
 
     // Start is called before the first frame update
     void Start()
@@ -43,6 +44,23 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        canJump = true;
+        if(other.gameObject.tag != "Bounds") canJump = true;
+        if(other.gameObject.tag == "Obstacle")
+        {
+            playerRb.velocity = Vector3.zero;
+            canJump = false;
+            transform.position = startPos;
+        }
+        if(other.gameObject.tag == "Finish")
+        {
+            playerRb.velocity = Vector3.zero;
+            canJump = false;
+            transform.position = startPos;
+        }
+        if(other.gameObject.tag == "Bouncy")
+        {
+            canJump = false;
+            playerRb.AddForce(0f, 600f, 0f);
+        }
     }
 }
